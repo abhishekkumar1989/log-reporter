9e53b3c3477c4714fa91daa223eabdf9dfecaade
Abhishek Kumar <abhishek.k@directi.com>
2017-03-30 08:43:47 +0530

    file test change

--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 export MAVEN_OPTS="-Xms128m -Xmx512m -XX:PermSize=128M -XX:MaxPermSize=256M -XX:PermSize=256m -XX:MaxPermSize=256m -Xdebug -Xnoagent -Djava.compiler=NONE -Djava.library.path=/usr/lib:/usr/local/lib -Xrunjdwp:transport=dt_socket,address=4010,server=y,suspend=n"
 mvn -e clean jetty:run $@
-
+#New comment
